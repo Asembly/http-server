07e415723c359101402560cb82b6e1d1cb346be6
asmbly <darklayergd@gmail.com>
2026-03-31 22:16:40 +0300

    feat: Add new response methods

--- src/main/java/asembly/httpserver/http/ResponseFabric.java
+++ src/main/java/asembly/httpserver/http/ResponseFabric.java
@@ -25,4 +25,37 @@ public class ResponseFabric {
                .build();
     }
 
+    public static Response badRequest(byte[] body)
+    {
+        return new Response.Builder()
+                .body(body)
+                .statusCode(400)
+                .addHeader("Connection", "close")
+                .addHeader("Content-Length", String.valueOf(body.length))
+                .addHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    public static Response internalError(byte[] body)
+    {
+        return new Response.Builder()
+                .body(body)
+                .statusCode(500)
+                .addHeader("Connection", "close")
+                .addHeader("Content-Length", String.valueOf(body.length))
+                .addHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    public static Response forbidden(byte[] body)
+    {
+        return new Response.Builder()
+                .body(body)
+                .statusCode(403)
+                .addHeader("Connection", "close")
+                .addHeader("Content-Length", String.valueOf(body.length))
+                .addHeader("Content-Type", "application/json")
+                .build();
+    }
+
 }
